23d987b6bac6693b6f176bec66c07cad61bc6779
Alice Wonder <github@domblogger.net>
2018-05-14 10:33:54 -0700

    phpunit tests PunycodeStatic

---
+++ tests/PunycodeStaticTest.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Unit testing for SimpleCache abstract class. This not only tests the concrete
+ * methods that do not depend upon cache implementation but it can serve as a template
+ * for implementation unit tests, just do not create the anonymous class.
+ *
+ * @package AWonderPHP
+ * @author  Alice Wonder <paypal@domblogger.net>
+ * @license https://opensource.org/licenses/MIT MIT
+ * @link    https://github.com/AliceWonderMiscreations/AWonderPHP
+ */
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test class for SimpleCache no strict no encryption
+ */
+// @codingStandardsIgnoreLine
+final class PunycodeStaticTest extends TestCase
+{
+    public function testPunycodeDomain(): void
+    {
+        $domains = array(
+            'österreich.icom.museum' => 'xn--sterreich-z7a.icom.museum',
+            'србијаицрнагора.иком.museum' => 'xn--80aaabm1ab4blmeec9e7n.xn--h1aegh.museum',
+            '価格.com' => 'xn--1sqt31d.com',
+            'example.org' => 'example.org'
+        );
+        foreach($domains as $idn => $expected) {
+            $actual = \AWonderPHP\PluggableUnplugged\PunycodeStatic::punycodeDomain($idn);
+            $this->assertEquals($expected, $actual);
+        }
+    }
+    
+    public function testUnpunycodeDomain(): void
+    {
+        $domains = array(
+            'xn--sterreich-z7a.icom.museum' => 'österreich.icom.museum',
+            'xn--80aaabm1ab4blmeec9e7n.xn--h1aegh.museum' => 'србијаицрнагора.иком.museum',
+            'xn--1sqt31d.com' => '価格.com',
+            'example.org' => 'example.org'
+        );
+        foreach($domains as $ascii => $expected) {
+            $actual = \AWonderPHP\PluggableUnplugged\PunycodeStatic::unpunycodeDomain($ascii);
+            $this->assertEquals($expected, $actual);
+        }
+    }
+    
+    public function testPunycodeEmail(): void
+    {
+        $addresses = array(
+            'user@österreich.icom.museum' => 'user@xn--sterreich-z7a.icom.museum',
+            'user@србијаицрнагора.иком.museum' => 'user@xn--80aaabm1ab4blmeec9e7n.xn--h1aegh.museum',
+            'user@価格.com' => 'user@xn--1sqt31d.com',
+            'user@example.org' => 'user@example.org'
+        );
+        foreach($addresses as $idn => $expected) {
+            $actual = \AWonderPHP\PluggableUnplugged\PunycodeStatic::punycodeEmail($idn);
+            $this->assertEquals($expected, $actual);
+        }
+    }
+}
